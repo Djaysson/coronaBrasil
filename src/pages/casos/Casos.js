9b89d073ef97e6fc5fefe03075e393e83a86ba32
Djaysson <djayson.dev@gmail.com>
2020-11-25 00:43:26 -0300

    Component creation of the Brazilian states table completed

--- src/pages/casos/Casos.js
+++ src/pages/casos/Casos.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
-
+import TabelaEstadosBrasil from '../../components/casosTabela/TabelaEstadosBrasil';
+import backgroundImg from '../../images/background.png';
+import './styles.css';
 const Casos = () => {
   return (
-    <div>
-      <h1>Página Casos está em desenvolvimento</h1>
+    <div className="container_casos">
+      <img src={backgroundImg} alt="Plano de fundo" />
+      <TabelaEstadosBrasil />
     </div>
   );
 }
