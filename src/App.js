d588dc70f313218993193ec76429dc980dca529f
Djaysson <djayson.dev@gmail.com>
2020-12-01 13:55:58 -0300

    Creating the footer component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import '../src/global.css';
 import Header from './components/header/Header';
 import Routes from './Routes';
 import { BrowserRouter } from "react-router-dom";
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
         <Routes />
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./styles.css";
+
+
+function Footer() {
+  return (
+    <div className="footer">
+      <footer> &copy; Copyright 2020-All Rights Reserved - Teste covid </footer>
+    </div >
+  );
+}
+export default Footer;
