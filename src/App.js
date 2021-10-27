0bd4695a16002e1cb28d0bb7ed3acea6e67f0f16
Djaysson <djayson.dev@gmail.com>
2021-10-27 20:49:20 -0300

    Updating files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import '../src/global.css';
-import Header from './components/header/Header';
-import Routes from './Routes';
+import React from "react";
+import "../src/global.css";
+import Header from "./components/header/Header";
+import Routes from "./Routes";
 import { BrowserRouter } from "react-router-dom";
-import Footer from './components/footer/Footer';
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
-        <Routes />
+        {/* <Routes /> */}
         <Footer />
       </BrowserRouter>
     </div>
