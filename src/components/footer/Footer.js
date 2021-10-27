7b255339fdf09e3a3b8dfe849d3dd08bc317a71e
Djaysson <djayson.dev@gmail.com>
2021-10-27 20:47:52 -0300

    Refactoring the header component and footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,15 @@
-import React from 'react';
-
+import React from "react";
+import ImgLogo from "../../images/logo.png";
 import "./styles.css";
-
 
 function Footer() {
   return (
     <div className="footer">
-      <footer> &copy; Copyright 2020-All Rights Reserved - Teste covid </footer>
-    </div >
+      <footer>
+        <h2>Desenvolvido por Djayson Rodrigues - </h2>
+        <img src={ImgLogo} alt="logo" />
+      </footer>
+    </div>
   );
 }
 export default Footer;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,32 +1,55 @@
-import React from 'react';
-import { FiAlignJustify } from "react-icons/fi";
-import './styles.css';
-import LogoImg from '../../images/logo.svg';
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { MdMenu, MdClose } from "react-icons/md";
+import LogoImg from "../../images/logo.png";
+import { NavLink } from "react-router-dom";
+import "./styles.css";
 
-function header() {
+function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
-    <div className="container">
-      <header>
-        <img src={LogoImg} alt="logo" />
-        <nav>
-          <input type="checkbox" id="check" />
-          <label htmlFor="check" className="checkbtn">
-            <FiAlignJustify />
-          </label>
+    <header className="container">
+      <img src={LogoImg} alt="logo" />
 
-
-          <ul>
-            <li><Link to="/">Casos</Link></li>
-            <li><Link to="/Tests">Testes</Link></li>
-            <li><Link to="/Tips">Dicas</Link></li>
-            <li><Link to="/Plato">Plantão Coronavirus</Link></li>
-          </ul>
-
-        </nav>
-      </header>
-    </div>
+      <div className="navMobile">
+        <button onClick={handleOpenMenu}>
+          {openMenu !== true ? (
+            <MdMenu size={40} color="#EFF3F4" />
+          ) : (
+            <MdClose size={40} color="#EFF3F4" />
+          )}
+        </button>
+      </div>
+      <nav id="navContainer" className={openMenu !== true ? "" : "open"}>
+        <ul>
+          <li onClick={handleOpenMenu}>
+            <NavLink to="/" exact activeStyle={{ color: "#7DB86B" }}>
+              Home
+            </NavLink>
+          </li>
+          <li onClick={handleOpenMenu}>
+            <NavLink to="/Tests" activeStyle={{ color: "#7DB86B" }}>
+              Status Casos
+            </NavLink>
+          </li>
+          <li onClick={handleOpenMenu}>
+            <NavLink to="/Tips" activeStyle={{ color: "#7DB86B" }}>
+              Status vacinação
+            </NavLink>
+          </li>
+          <li onClick={handleOpenMenu}>
+            <NavLink to="/Plato" activeStyle={{ color: "#7DB86B" }}>
+              Dicas
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
-export default header;
+export default Header;
